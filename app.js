79e383c2765a441504a5cf6871f0b8ed58674946
Tyler Berry <tyler@berryac.com>
2016-06-13 10:44:58 -0600

    express hello world listening on 8080

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+var PORT = 8080;
+
+app.get('/compile', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, function () {
+  console.log('Example app listening on port ' + PORT);
+});
