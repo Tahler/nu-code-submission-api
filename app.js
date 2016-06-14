4fde4e1d46604cdaef371cac095bf58b894657fa
Tyler Berry <tyler@berryac.com>
2016-06-13 22:04:46 -0600

    added documentation to help make the web api

--- app.js
+++ app.js
@@ -3,6 +3,22 @@ var app = express();
 
 var PORT = 8080;
 
+/**
+ * Details the Web API for compilation.
+ *
+ * Requests should be sent to this API as JSON, containing two fields:
+ * - "language": A string specifying the language code of the source code. A full list can be found
+ *   in another file. // TODO!
+ * - "code": A string containing all the source code.
+ *
+ * Responses are sent as JSON.
+ * If the request was correctly formatted, then the JSON response will contain two fields:
+ * - "wasSuccess": A boolean: true if the source code successfully compiled. False otherwise.
+ * - "output": A string containing the output of the program. If the program failed to compile,
+ *   errors are reported here.
+ * Otherwise, the JSON response will contain a single field:
+ * - "error": A string detailing the error in the request.
+ */
 app.get('/compile', function (req, res) {
   res.send('Hello World!');
 });
