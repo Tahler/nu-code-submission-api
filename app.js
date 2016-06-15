3cae80ef9d86d961b9459e881c628851fec38867
Tyler Berry <tyler@berryac.com>
2016-06-15 11:09:07 -0600

    more readable req format checking

--- app.js
+++ app.js
@@ -12,12 +12,12 @@ const PORT = 8080;
  * Details the Web API for compilation.
  *
  * Requests should be sent to this API as a post request containing JSON with two fields:
- * - "language": A string specifying the language code of the source code. A full list can be found
+ * - "lang": A string specifying the language code of the source code. A full list can be found
  *   in another file. // TODO!
- * - "code": A string containing all the source code.
+ * - "src": A string containing all of the source code.
  *
  * Example request using cURL:
- * curl -d '{"language": "c", "code": "xxx"}' -H "Content-Type: application/json" http://domain/code
+ * curl -d '{"lang": "c", "src": ""}' -H "Content-Type: application/json" http://localhost:8080/code
  *
  * Responses are sent as JSON.
  * If the request was correctly formatted, then the JSON response will contain two fields:
@@ -28,18 +28,24 @@ const PORT = 8080;
  * - "error": A string detailing the error in the request.
  */
 app.post('/code', jsonParser, function (req, res) {
+  const LANGUAGE_CODE_PROPERTY = 'lang';
+  const SRC_PROPERTY = 'src';
   const jsonReq = req.body;
 
-  if (jsonReq) {
-    if (jsonReq.hasOwnProperty('language')
-        && jsonReq.hasOwnProperty('code')) {
-      const lang = jsonReq.language;
-      const code = jsonReq.code;
+  function reqIsFormattedCorrectly(req) {
+    return req
+      && req.hasOwnProperty(LANGUAGE_CODE_PROPERTY)
+      && req.hasOwnProperty(SRC_PROPERTY);
+  }
 
-      res.send('API correct\n'); // TODO use the script, then configure running that in docker
-    }
+  if (reqIsFormattedCorrectly(jsonReq)) {
+    const lang = jsonReq.LANGUAGE_CODE_PROPERTY;
+    const code = jsonReq.SRC_PROPERTY;
+
+    res.send('Correct\n'); // TODO use the script, then configure running that in docker
   } else {
-    res.sendStatus(400);
+    const BAD_REQUEST_STATUS_CODE = 400;
+    res.sendStatus(BAD_REQUEST_STATUS_CODE);
   }
 });
 
