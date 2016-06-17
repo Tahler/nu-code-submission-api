d302d614071e842a26bd0bcdef3f047ca60e8d3d
Tyler Berry <tyler@berryac.com>
2016-06-16 18:15:10 -0600

    api now receives and sends time parameters

--- app.js
+++ app.js
@@ -11,87 +11,90 @@ const PORT = 8080;
 /**
  * Details the Web API for compilation.
  *
- * Requests should be sent to this API as a post request containing JSON with two fields:
+ * Requests should be sent to this API as a post request containing JSON with three fields:
  * - "lang": A string specifying the language code of the source code. A full list can be found in
  *   the properties of compilers.js.
  * - "src": A string containing all of the source code.
+ * - "seconds": A number specifying the amount of seconds before a timeout is reported.
  *
  * Example request using cURL:
- * curl -d '{"lang": "c", "src": "#include <stdio.h>\n\nint main()\n{\n  printf(\"Hello, world!\");\n}"}' -H "Content-Type: application/json" http://localhost:8080/code
+ * curl -d '{"lang": "c", "src": "#include <stdio.h>\n\nint main()\n{\n  printf(\"Hello, world!\");\n}", "seconds": 10}' -H "Content-Type: application/json" http://localhost:8080/code
  *
  * Responses are sent as JSON.
- * If the request was correctly formatted, then the JSON response will contain two fields:
- * - "wasSuccess": A boolean: true if the source code successfully compiled. False otherwise.
+ * If the request was correctly formatted, then the JSON response will contain two properties:
+ * - "status": A string:
+ *    - "success" if the source code successfully compiled and ran.
+ *    - "error" if the source code contained errors.
+ *    - "timeout" if the time length specified in seconds was exceeeded.
  * - "output": A string containing the output of the program. If the program failed to compile,
  *   errors are reported here.
- * Otherwise, the JSON response will contain a single field:
+ * Otherwise, the JSON response will contain a single property:
  * - "error": A string detailing the error in the request.
  */
 app.post('/code', jsonParser, function (req, res) {
   const LANGUAGE_CODE_PROPERTY = 'lang';
   const SRC_PROPERTY = 'src';
+  const TIMEOUT_PROPERTY = 'seconds';
+
   const jsonReq = req.body;
 
-  function reqIsFormattedCorrectly(req) {
+  function reqHasRequiredFields(req) {
     return req
       && req.hasOwnProperty(LANGUAGE_CODE_PROPERTY)
-      && req.hasOwnProperty(SRC_PROPERTY);
+      && req.hasOwnProperty(SRC_PROPERTY)
+      && req.hasOwnProperty(TIMEOUT_PROPERTY);
   }
 
   function langIsSupported(lang) {
     return compilers.hasOwnProperty(lang);
   }
 
-  if (reqIsFormattedCorrectly(jsonReq)) {
+  if (reqHasRequiredFields(jsonReq)) {
     const lang = jsonReq[LANGUAGE_CODE_PROPERTY];
     const code = jsonReq[SRC_PROPERTY];
-    if (langIsSupported(lang)) {
-      execute(lang, code, function (result, err) {
-        if (err) {
-          console.log('UH OH! That should not have happened!');
-        } else {
-          resultJson = JSON.stringify(result);
-          res.send(resultJson);
-        }
-      });
+    const seconds = jsonReq[TIMEOUT_PROPERTY];
+
+    if (seconds > 0) {
+      if (langIsSupported(lang)) {
+        // Request can be executed.
+        execute(lang, code, seconds, function (result) {
+          res.send(result);
+        });
+      } else {
+        const langUnsupportedErr = '{"error": "Language ' + lang + ' is unsupported."}\n';
+        res.send(langUnsupportedErr);
+      }
     } else {
-      const langUnsupportedErr = '{ "error": "Language ' + lang + ' is unsupported." }\n';
-      res.send(langUnsupportedErr);
+      const negSecondsErr = '{"error": "The \\"seconds\\" property must be positive."}\n';
+      res.send(negSecondsErr);
     }
   } else {
-    const badRequestErr = '{ "error": "Requests must be sent as JSON containing two fields: \\"lang\\" and \\"src\\"." }\n';
+    const badRequestErr = '{"error": "Requests must be sent as JSON containing three properties: \\"lang\\", \\"src\\" and \\"seconds\\"."}\n';
     res.send(badRequestErr);
   }
 });
 
 function writeToFile(filename, buffer, callback) {
   const fs = require('fs');
-  fs.writeFile(filename, buffer, function (err) {
-    callback(err);
+  fs.writeFile(filename, buffer, function () {
+    callback();
   });
 }
 
-function execute(lang, code, callback) {
+function execute(lang, code, seconds, callback) {
   const parameters = compilers[lang];
   const filename = parameters[1];
 
-  writeToFile(filename, code, function (err) {
+  writeToFile(filename, code, function () {
     const exec = require('child_process').exec;
 
-    const cmd = './compile.sh ' + parameters.join(' ');
+    const cmd = './compile.sh ' + seconds + ' ' + parameters.join(' ');
     exec(cmd, function (error, stdout, stderr) {
-      const wasSuccess = stderr == "";
-      const output = wasSuccess ? stdout : stderr;
-      const result = {
-        wasSuccess: wasSuccess,
-        // execTime: execTime,
-        output: output
-      };
-      callback(result, err);
+      callback(stdout);
     });
   });
 }
 
 app.listen(PORT, function () {
-  console.log('Example app listening on port ' + PORT);
+  console.log('Listening on port ' + PORT);
 });
