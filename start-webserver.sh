76a08f7b087f40326b5152d2c9b69caf6b01d37a
Tyler Berry <tyler@berryac.com>
2016-06-26 17:46:44 -0600

    constants in start-webserver.sh

--- start-webserver.sh
+++ start-webserver.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-# TODO: extract constants, especially port
+# Internal Port must match PORT in app.js
+INTERNAL_PORT=8080
+EXTERNAL_PORT=8080
+NAME=web
+IMAGE=web
+DOCKER_SOCKET_PATH=/var/run/docker.sock
 
-docker run --rm --name web -p 8080:8080 -v /var/run/docker.sock:/var/run/docker.sock web
+docker run -d --name $NAME -p $EXTERNAL_PORT:$INTERNAL_PORT \
+           -v $DOCKER_SOCKET_PATH:$DOCKER_SOCKET_PATH web
