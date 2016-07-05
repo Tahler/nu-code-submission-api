8fdd73f77823ba88e9d924577d950e7175419fb9
Tyler Berry <tyler@berryac.com>
2016-07-05 09:32:01 -0600

    fixed bug in run.sh, updated docs

--- run.sh
+++ run.sh
@@ -7,8 +7,8 @@
 #   1. The number of seconds before timing out.
 #		2. The command to execute the object code.
 #   3. The name of the input file to redirect into the program.
-#   4. (Optional) The name of the output file that should hold the outputted
-#      results. If unspecified, the output will be returned as JSON data. TODO: here
+#   4. The name of the output file that should hold the outputted
+#      results.
 #	Example Usage:
 #   ./run.sh 1 "./a.out" input.txt output.txt
 #   ./run.sh 3 "nodejs solution.js" input.txt output.txt
@@ -34,6 +34,7 @@ EXIT_CODE=${PIPESTATUS[0]}
 END_TIME=$(date $DATE_EXPRESSION)
 TOTAL_TIME=$(echo "$END_TIME - $START_TIME" | bc)
 
+SUCCESS_CODE=0
 # 124 is the exit code for a timeout
 TIMEOUT_CODE=124
 
@@ -41,7 +42,7 @@ case $EXIT_CODE in
   $SUCCESS_CODE)
     # Successful run
     # Report total exec time
-    echo $TOTAL_TIME
+    echo "$TOTAL_TIME"
     ;;
   $TIMEOUT_CODE)
     # Timeout
