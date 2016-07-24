69cc5255b969adcc662ff7eb5082d2a777b4bfff
Tyler Berry <tyler@berryac.com>
2016-07-23 22:31:38 -0600

    updated comment in supported-compilers.js

--- supported-compilers.js
+++ supported-compilers.js
@@ -1,14 +1,8 @@
 /*
  * This file contains a single exported object which acts as a map.
  *
- * Each key-value pair creates another supported language.
+ * Each property creates another supported language.
  * Thus, to support a new language, adding to this file should be the only necessary step.
- *
- * Each key maps to a string array of "compiler arguments".
- * - The 1st column contains the compiler / interpretor that is used for translation.
- * - The 2nd column is the file name of the source code.
- * - The 3rd column is needed only for compiled languages: it contains the command to invoke the
- *   compiled program.
  */
 
 module.exports = {
