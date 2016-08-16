1d9fda37ed623f41993cf939f2543a9c2bb8ed22
Tyler Berry <tyler@berryac.com>
2016-08-15 23:09:25 -0600

    should validate requests before accepting them

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,13 @@ import { Promise } from 'es6-promise';
 import * as express from 'express';
 
 import {
-  FirebasePathDoesNotExistError,
+  CompetitionDoesNotExistError,
+  CompetitionEndedError,
+  CompetitionNotStartedError,
   InvalidRequestError,
   LanguageUnsupportedError,
   NoTokenError,
-  ProblemDoesNotExistError,
-  UnexpectedError
+  ProblemDoesNotExistError
 } from './errors';
 import { Firebase } from './firebase';
 import { HttpStatusCodes } from './http-status-codes';
@@ -38,7 +39,7 @@ app.use((req, res, next) => {
 /**
  * To be posted when a user is newly verified.
  *
- * Retrieves all of that user's successful submissions and
+ * Retrieves all of that user's successful submissions and moves them to
  */
 app.post('/verified', jsonParser, (req, res) => {
   let request = req.body;
@@ -57,54 +58,79 @@ app.post('/verified', jsonParser, (req, res) => {
 app.post('/api', jsonParser, (req, res) => {
   let request: Request = req.body;
   request.submittedOn = new Date().getTime();
-  if (Request.hasRequiredProperties(request)) {
-    let lang = request.lang;
-    if (langIsSupported(request.lang)) {
-      handleRequest(request, res);
-    } else {
-      res.status(HttpStatusCodes.BadRequest).send(new LanguageUnsupportedError(lang));
-    }
-  } else {
-    res.status(HttpStatusCodes.BadRequest).send(new InvalidRequestError(request));
-  }
+  handleRequest(request, res);
 });
 
-function handleRequest(request: Request, res: express.Response) {
-  // Retreve the needed info from Firebase
-  // Load the timeout and test cases asynchronously
-  let timeoutLocation = request.competition
-      ? `/competitionProblems/${request.competition}/${request.problem}/timeout`
-      : `/problems/${request.problem}/timeout`;
-  Promise.all([
-    Firebase.get(timeoutLocation),
-    Firebase.get(`/tests/${request.problem}`)
-  ]).then(
-    values => {
-      let timeout = values[0];
-      let tests = values[1];
+/**
+ * Throws error if any are found. Returns true if valid.
+ * I hate this design.
+ */
+async function validateRequest(request: Request): Promise<boolean> {
+  if (!Request.hasRequiredProperties(request)) {
+    throw new InvalidRequestError(request);
+  }
+  if (!langIsSupported(request.lang)) {
+    throw new LanguageUnsupportedError(request.lang);
+  }
 
-      // Ready to execute code
-      let runner = new Runner(request.lang, request.src, timeout, tests);
-      runner.run().then(
-        result => {
-          if (request.submitterToken) {
-            Firebase.recordResult(request, result);
-          }
-          res.status(HttpStatusCodes.Success).send(result);
-        },
-        err => {
-          res.status(HttpStatusCodes.ServerError).send(UnexpectedError);
-          console.error(err);
-        });
-    },
-    err => {
-      if (err instanceof FirebasePathDoesNotExistError) {
-        res.status(HttpStatusCodes.BadRequest).send(new ProblemDoesNotExistError(request.problem));
-      } else {
-        res.status(HttpStatusCodes.ServerError).send(UnexpectedError);
-        console.error(err);
-      }
-    });
+  let problemId = request.problem;
+  let competitionId = request.competition;
+
+  if (competitionId === undefined) {
+    let problemExists = await Firebase.problemExists(problemId);
+    if (!problemExists) {
+      throw new ProblemDoesNotExistError(problemId);
+    }
+  } else {
+    let competitionExists = await Firebase.competitionExists(competitionId);
+    if (!competitionExists) {
+      throw new CompetitionDoesNotExistError(competitionId);
+    }
+
+    let competitionHasStarted =
+        await Firebase.competitionStartTimeBefore(competitionId, request.submittedOn);
+    if (!competitionHasStarted) {
+      throw new CompetitionNotStartedError(competitionId);
+    }
+
+    let competitionHasEnded =
+        await Firebase.competitionEndTimeAfter(competitionId, request.submittedOn);
+    if (competitionHasEnded) {
+      throw new CompetitionEndedError(competitionId);
+    }
+
+    let problemExists = await Firebase.competitionProblemExists(competitionId, problemId);
+    if (!problemExists) {
+      throw new ProblemDoesNotExistError(problemId);
+    }
+  }
+
+  return true;
+}
+
+async function handleRequest(request: Request, res: express.Response): Promise<void> {
+  try {
+    // This will throw errors if not valid.
+    await validateRequest(request);
+    // Retreve the needed info from Firebase
+    // Load the timeout and test cases asynchronously
+    let timeoutLocation = request.competition
+        ? `/competitionProblems/${request.competition}/${request.problem}/timeout`
+        : `/problems/${request.problem}/timeout`;
+    let [timeout, tests] = await Promise.all([
+      Firebase.get(timeoutLocation),
+      Firebase.get(`/tests/${request.problem}`)
+    ]);
+    // Ready to execute code
+    let runner = new Runner(request.lang, request.src, timeout, tests);
+    let result = await runner.run();
+    if (request.submitterToken) {
+      Firebase.recordResult(request, result);
+    }
+    res.status(HttpStatusCodes.Success).send(result);
+  } catch (err) {
+    res.status(HttpStatusCodes.BadRequest).send(err);
+  }
 }
 
 app.listen(Port, () => console.log(`Listening on port ${Port}`));
